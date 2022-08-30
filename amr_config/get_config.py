8681f5d1127a7c004f8b4f4a953b3f5691dca1b4
Tony Najjar <tony.najjar@logivations.com>
2022-08-30 17:11:28 +0200

    .

--- amr_config/get_config.py
+++ amr_config/get_config.py
@@ -2,33 +2,42 @@
 
 import collections.abc
 import yaml
+from .get_w2mo_config import get_w2mo_config
 
 def update(d, u):
-    for k, v in u.items():
-        if isinstance(v, collections.abc.Mapping):
-            d[k] = update(d.get(k, {}), v)
-        else:
-            d[k] = v
+    if u is not None:
+        for k, v in u.items():
+            if isinstance(v, collections.abc.Mapping):
+                d[k] = update(d.get(k, {}), v)
+            else:
+                d[k] = v
     return d
 
-if __name__ == "__main__":
-    with open("/root/amr_config/amr6_instance/instance.yaml", 'r') as stream:
+def get_config():
+    with open("/code/ros2_ws/src/amr_config/instance/instance.yaml", 'r') as stream:
         try:
             instance_yaml=yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
-    with open("/root/amr_config/brummer_environment/environment.yaml", 'r') as stream:
+    with open("/code/ros2_ws/src/amr_config/environment/environment.yaml", 'r') as stream:
         try:
             environment_yaml=yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
-    with open("/root/amr_config/defaults.yaml", 'r') as stream:
+    with open("/code/ros2_ws/src/amr_config/defaults.yaml", 'r') as stream:
         try:
             default_yaml=yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
+    w2mo_config = get_w2mo_config()
+    environment_yaml = update(environment_yaml, w2mo_config) # Assuming w2mo parameters only update environment values
+
     config = update(update(default_yaml, environment_yaml), instance_yaml)
-    print(config)
+    with open('/code/ros2_ws/src/amr_config/active_config.yaml', 'w') as outfile:
+        yaml.dump(config, outfile, default_flow_style=False)
+
+if __name__ == "__main__":
+    get_config()
---
+++ amr_config/get_w2mo_config.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def get_w2mo_config():
+    return {}
